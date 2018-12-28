78cfb221a30ce5310e7ec9f1bbac1063db1b17ed
denzilly <btimmer313@gmail.com>
2018-12-28 10:58:09 +0630

    added input validation, cleaned up formatting

--- motor.py
+++ motor.py
@@ -1,83 +1,154 @@
 import time
 import RPi.GPIO as GPIO
 
-GPIO.setmode(GPIO.BCM)
+def setup():
 
-#output pins
-pins = [17,18,22,23]
-Enable_293 = 2
+    GPIO.setmode(GPIO.BCM)
 
-
-#Initialize motor driver GPIO pins to state 1 (L293D IC interprets 1 as zero state)
-
-for pin in pins:
-    GPIO.setup(pin, GPIO.OUT)
-    GPIO.output(pin, 1)
-
-#Initialize enable pin to state 0
-GPIO.setup(Enable_293, GPIO.OUT)
-GPIO.output(Enable_293, 0)
-
-#Wait x seconds
-time.sleep(1)
+    #output pins
+    pins = [17,18,22,23]
+    Enable_293 = 2
 
 
+    #Initialize motor driver GPIO pins to state 1 (L293D IC interprets 1 as zero state)
 
-#These are the pulse sequences that define how the motor turns, in half steps
+    for pin in pins:
+        GPIO.setup(pin, GPIO.OUT)
+        GPIO.output(pin, 1)
 
-sequence_ccw =  [ [1,0,1,1],
-             [1,0,1,0],
-             [1,1,1,0],
-             [0,1,1,0],
-             [0,1,1,1],
-             [0,1,0,1],
-             [1,1,0,1],
-             [1,0,0,1], ]
+    #Initialize enable pin to state 0
+    GPIO.setup(Enable_293, GPIO.OUT)
+    GPIO.output(Enable_293, 0)
 
-sequence_cw =  [ [1,0,0,1],
-             [1,1,0,1],
-             [0,1,0,1],
-             [0,1,1,1],
-             [0,1,1,0],
-             [1,1,1,1],
-             [1,0,1,0],
-             [1,0,1,1], ]
+    #Wait x seconds
+    time.sleep(1)
 
 
 
+########This function requests rotation, speed, and direction inputs from the user, and validates input types.
+def get_params():
 
-for pin in pins:
-    print("Pin %s status is: " % (str(pin)) + str(GPIO.input(pin)))
+#define errors
+    class NotPositiveError(UserWarning):
+        pass
+    class NotCWError(UserWarning):
+        pass
 
-print("Pin %s status is: " % (str(Enable_293)) + str(GPIO.input(Enable_293)))
+#request degrees of rotation, validate input
+    while True:
+        try:
+            rot = int(input("How many degrees of rotation?"))
+            if (rot < 0):
+                raise NotPositiveError
+            print(rot)
+            break
+
+        except ValueError:
+            print ("Please enter a positive integer value!")
+            continue
+
+        except NotPositiveError:
+            print ("Your value was negative!")
+            continue
+
+        else:
+            print(rot)
+            break
+#Request rotation speed, validate input
+    while True:
+        try:
+            spd = float(input("Rotation speed (time between pulses .002-.5)?"))
+            if (spd < 0):
+                raise NotPositiveError
+            break
+
+        except ValueError:
+            print ("Please enter a positive decimal value!")
+            continue
+
+        except NotPositiveError:
+            print ("Your value was negative!")
+            continue
+
+#Request rotation direction, validate input
+    while True:
+        try:
+            di = str(input("Direction of rotation? (cw or ccw)"))
+            if (di != "ccw") & (di != "cw"):
+                raise NotCWError
+
+            break
+
+        except ValueError:
+            print ("Please enter ccw or cw")
+            continue
+
+        except NotCWError:
+            print ("Please enter ccw or cw")
+            continue
+
+    params = [rot, spd, di]
+
+    return params
 
 
-GPIO.output(Enable_293, 1)
-
-print("Enable 293D pin is live!")
-
-time.sleep(1)
-
-rot = float(input("How many degrees of rotation?"))
-spd = float(input("Rotation speed? (time between pulses)"))
-di = input("Direction of rotation? (cw or ccw)")
+def runmotor():
 
 
-if (di == "cw"):
-    direction = sequence_cw
-else:
-    direction = sequence_ccw
-    
-step_count = round(rot/2.8125)
+    #Check status of all pins
+    for pin in pins:
+        print("Pin %s status is: " % (str(pin)) + str(GPIO.input(pin)))
 
-for i in range(step_count):
-    for half_step in range(8):
-        for pin in range(4):
-            GPIO.output(pins[pin], direction[half_step][pin])
-        #for pin in pins:
-            #print("Pin %s status is: " % (str(pin)) + str(GPIO.input(pin)))
-        time.sleep(spd)
+    print("Pin %s status is: " % (str(Enable_293)) + str(GPIO.input(Enable_293)))
 
+    #Activate enable pin
+    GPIO.output(Enable_293, 1)
+
+    print("Enable 293D pin is live!")
+
+    time.sleep(1)
+
+    #These are the pulse sequences that define how the motor turns, in half steps
+
+    sequence_ccw =  [ [1,0,1,1],
+                 [1,0,1,0],
+                 [1,1,1,0],
+                 [0,1,1,0],
+                 [0,1,1,1],
+                 [0,1,0,1],
+                 [1,1,0,1],
+                 [1,0,0,1], ]
+
+    sequence_cw =  [ [1,0,0,1],
+                 [1,1,0,1],
+                 [0,1,0,1],
+                 [0,1,1,1],
+                 [0,1,1,0],
+                 [1,1,1,1],
+                 [1,0,1,0],
+                 [1,0,1,1], ]
+
+
+    #Get parameters from user as list [rot, spd, di]
+    params = get_params()
+
+    if (params[2] == "cw"):
+        direction = sequence_cw
+    else:
+        direction = sequence_ccw
+
+    step_count = round(params[0]/2.8125)
+
+    for i in range(step_count):
+        for half_step in range(8):
+            for pin in range(4):
+                GPIO.output(pins[pin], direction[half_step][pin])
+            #for pin in pins:
+                #print("Pin %s status is: " % (str(pin)) + str(GPIO.input(pin)))
+            time.sleep(params[1])
+
+setup()
+runmotor()
 
 
 print("Sequence Complete")
